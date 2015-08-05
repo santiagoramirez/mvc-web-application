7340b4a83eef4e5288d1443a1443cf9b61e41dcf
Santiago Ramirez <hello@santiagoramirez.net>
2015-08-05 19:11:03 -0400

    Add requires

--- resources/config.php
+++ resources/config.php
@@ -7,17 +7,28 @@
  * and defines commonly used constants.
  */
 
+require_once 'functions.php';
+
 define('DB_NAME','');
+
 define('DB_HOSTNAME', '');
+
 define('DB_USERNAME', '');
+
 define('DB_PASSWORD', '');
+
 define('TABLE_PREFIX', '');
 
 define('DOMAIN_ROOT', '');
+
 define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT']);
 
 define('MAINTENANCE_MODE', false);
 
-require_once 'functions.php';
+require_once locate('core/base-controller.php');
+require_once locate('core/base-model.php');
+require_once locate('core/base-view.php');
+require_once locate('resources/classes/secure-session.php');
+require_once locate('resources/libs/smarty/smarty.php');
 
 ?>
