4422bad9fcd7227f879a82e18ce51d431be9cce8
Santiago Ramirez <hello@santiagoramirez.net>
2015-08-06 12:11:03 -0400

    Update indentation

--- resources/functions.php
+++ resources/functions.php
@@ -4,8 +4,7 @@
  * Locate a file when given the relative path.
  * @return string|false $absolute_path
  */
-function locate($relative_path)
-{
+function locate($relative_path) {
     $absolute_path = SERVER_ROOT.'/'.$relative_path;
     if (file_exists($absolute_path)) {
         return $absolute_path;
