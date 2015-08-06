11929e5ea587d39aed76cb5b5c45104e8be99142
Santiago Ramirez <hello@santiagoramirez.net>
2015-08-06 12:08:24 -0400

    Update Smarty path

--- resources/config.php
+++ resources/config.php
@@ -25,12 +25,18 @@ defined('TABLE_PREFIX')
 defined('DOMAIN_ROOT')
     or define('DOMAIN_ROOT', 'http://localhost/mvc-web-application');
 
+define('SERVER_ROOT')
+    or define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'].'/mvc-web-application');
 
+defined('MAINTENANCE_MODE')
+    or define('MAINTENANCE_MODE', false);
 
+require_once 'functions.php';
 require_once locate('core/base-controller.php');
 require_once locate('core/base-model.php');
 require_once locate('core/base-view.php');
 require_once locate('resources/classes/secure-session.php');
-require_once locate('resources/libs/smarty/smarty.php');
+require_once locate('resources/libs/smarty-3.1.27/smarty.php');
+
 
 ?>
