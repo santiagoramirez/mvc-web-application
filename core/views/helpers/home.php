c327eeb73b099d1474cb27ef31f6295da0267593
Santiago Ramirez <hello@santiagoramirez.net>
2015-08-06 12:58:09 -0400

    Create home.php

---
+++ core/views/helpers/home.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Our strings would normally be stored in the database, but for the sake of
+ * creating a helper, we're defining them here.
+ */
+
+$string = array('HELLO_WORLD' => 'Hello, World');
+$smarty->assign('STRING', $string);
+
+?>
